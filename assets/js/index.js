4505baf1accf8a166bd84078bb69aa9f292577b4
Soni Nishit <nishitsoni15>
2021-12-10 14:17:26 +0530

    header and footer commit

---
+++ assets/js/index.js
@@ -0,0 +1,22 @@
+$('document').ready(function(){
+    $('.clicked').click(function(){
+        $('.clicked').css({"background-color":"black","color":"white"});
+    })
+    $('.clicked1').click(function(){
+        $('.clicked1').css({"background-color":"black","color":"white"});
+    })
+    $('.clicked2').click(function(){
+        $('.clicked2').css({"background-color":"black","color":"white"});
+    })
+    $('.clicked3').click(function(){
+        $('.clicked3').css({"background-color":"black","color":"white"});
+    })
+})
+
+  
+
+
+
+
+
+
