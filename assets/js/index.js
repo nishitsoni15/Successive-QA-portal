0ba7c9d408e86d175c21c09f01dfd804b465b774
Soni Nishit <nishitsoni15>
2021-12-15 13:02:56 +0530

    updates

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,27 @@
 $('document').ready(function(){
-    $('.clicked').click(function(){
-        $('.clicked').css({"background-color":"black","color":"white"});
-    })
-    $('.clicked1').click(function(){
-        $('.clicked1').css({"background-color":"black","color":"white"});
-    })
-    $('.clicked2').click(function(){
-        $('.clicked2').css({"background-color":"black","color":"white"});
-    })
-    $('.clicked3').click(function(){
-        $('.clicked3').css({"background-color":"black","color":"white"});
+    $('.menu .a-1').click(function(){
+        $('.menu .a-2').removeClass('home');
+        $('.menu .a-3').removeClass('home');
+        $('.menu .a-4').removeClass('home');
+        $('.menu .a-1').toggleClass('home');
+    }),
+    $('.menu .a-2').click(function(){
+        $('.menu .a-1').removeClass('home');
+        $('.menu .a-3').removeClass('home');
+        $('.menu .a-4').removeClass('home');
+        $('.menu .a-2').toggleClass('home');
+    }),
+    $('.menu .a-3').click(function(){
+        $('.menu .a-1').removeClass('home');
+        $('.menu .a-2').removeClass('home');
+        $('.menu .a-4').removeClass('home');
+        $('.menu .a-3').toggleClass('home');
+    }),
+    $('.menu .a-4').click(function(){
+        $('.menu .a-1').removeClass('home');
+        $('.menu .a-2').removeClass('home');
+        $('.menu .a-3').removeClass('home');
+        $('.menu .a-4').toggleClass('home');
     })
 })
 
